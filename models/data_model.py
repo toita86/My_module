6d683661f30d6a27b39005a7ce3ba160441af917
Eduard Brahas <eduardbrhas@outlook.it>
2023-09-19 17:10:24 +0200

    Adding property for each field

--- models/data_model.py
+++ models/data_model.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from odoo import models, fields
 
 
@@ -5,8 +6,26 @@ class TestModel(models.Model):
     _name = "test_model"
     _description = "Test Model"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, default = "Unknown")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
-    selling_price = fields.Float(required=True)
+    date_availability = fields.Date(
+        copy = False,
+        default=lambda self:(datetime.now() + timedelta(days=90)).date()
+    )
+    check_box = fields.Boolean(default = False)
+    simple_integer = fields.Integer(default = 50)
+    choises_selecction = fields.Selection(
+        required = True,
+        copy = False,
+        selection = [
+                ('option 1','Option 1'),
+                ('option 2', 'Option 2'),
+                ('option 3','Option 3'),
+                ('option 4', 'Option 4'),
+            ],
+        string = 'List of Choises',
+        default = 'option1'
+    )
+    active_property = fields.Boolean(default = True)
+    selling_price = fields.Float(required=True, readonly = True, copy = False)
